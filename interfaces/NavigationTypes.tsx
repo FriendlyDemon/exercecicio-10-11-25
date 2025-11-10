940f0b24754a0ca96eed4dbf36e4240d7fac868c
PedroMachadoGRP <pedrolimass10@gmail.com>
2025-11-10 16:09:53 -0300

    add

--- interfaces/NavigationTypes.tsx
+++ interfaces/NavigationTypes.tsx
@@ -1,12 +1,40 @@
-import { ParamListBase } from "@react-navigation/native";
+import { ParamListBase, RouteProp } from "@react-navigation/native";
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 
-export interface StackParam extends ParamListBase{
-    HomeScreen: undefined;
-    EditProfileScreen:undefined;
-    SettingsScreen:undefined;
+
+export interface StackParamList extends ParamListBase {
+    EditProfileScreen: {nome:string, email:string,cidade:string};
+    TabNavigator:{nome:string, email:string,cidade:string, photo:string | undefined};
 }
 
-export interface HomeScreen{
-    navigation:NativeStackNavigationProp<StackParam,"Home">
+export interface BottomTabParamList extends ParamListBase {
+    HomeScreen: {nome:string, email:string,cidade:string, photo:string | undefined};
+    SettingsScreen: undefined;
+}
+
+export interface HomeScreen {
+    navigation: NativeStackNavigationProp<BottomTabParamList, "Home">
+    route:RouteProp<BottomTabParamList,"Home">
+}
+
+export interface EditProfileScreen {
+    navigation: NativeStackNavigationProp<BottomTabParamList, "EditProfile">
+    route:RouteProp<BottomTabParamList,"EditProfile">
+}
+
+export interface SettingsScreen {
+    navigation: NativeStackNavigationProp<StackParamList, "Settings">
+}
+
+export interface HomeScreenPropBottom {
+    navigation: BottomTabNavigationProp<BottomTabParamList, "Home">
+}
+
+export interface EditProfileScreenPropBottom {
+    navigation: BottomTabNavigationProp<BottomTabParamList, "EditProfile">
+}
+
+export interface SettingsScreenPropBottom {
+    navigation: BottomTabNavigationProp<BottomTabParamList, "Settings">
 }
